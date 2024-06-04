e6fc2871edee1105a89bb5837220d2c278da6dd0
Israel <israeldianga@gmail.com>
2024-06-04 13:07:12 +0300

    Other updates

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ app.get("/", (req, res) => {
 app.get("/recipe", async (req, res) => {
     try {
         const response = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php");
-        const result = response.data.drinks[0];
-        console.log(result);
+        const result = response.data.drinks[0];        
 
         res.render("index.ejs", { cocktail: result });
     } catch (error) {
